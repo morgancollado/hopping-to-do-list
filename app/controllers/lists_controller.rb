09f5f9b2fd77fd5d515f24aa7d496e92e1bdc82f
kanu-e <chukwuemekakanu@gmail.com>
2020-12-01 20:16:20 -0600

    added post fetch request

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,19 @@
 class ListsController < ApplicationController
     def index 
-        list = List.all
-        render json: list, only: [:name], include: [:items] 
+        lists = List.all
+        render json: lists, only: [:id, :name], include: [:items]    
     end 
+
+    def create
+        list = List.create(list_params)
+        render json: list, only: [:id, :name], include: [:items]    
+        # byebug
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name)
+    end
+
 end
